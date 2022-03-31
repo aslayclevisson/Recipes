282eb1a3605e29b39c210005de876e4a491e307d
Aslay Clevisson <aslayclei@gmail.com>
2022-03-31 09:38:42 -0300

    added new directory in templates

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app recipes
     'recipes.apps.RecipesConfig',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates'], # add other templates folder to the path
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
