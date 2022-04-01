d9e6e2b5ddb5af7486aa5e58c03b531cc0d1257c
Aslay Clevisson <aslayclei@gmail.com>
2022-04-01 19:14:52 -0300

    config: time zone adjusted

--- project/settings.py
+++ project/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'base_static',]
+
+STATIC_ROOT = BASE_DIR / 'static'  # creates a folder with all static files in the project (manage.py collectstatic)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
