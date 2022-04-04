3529fc9195838b415a0e563c87a3106c5ee9ce2a
Aslay Clevisson <aslayclei@gmail.com>
2022-04-03 23:00:44 -0300

    Fix: path

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from utils.recipe.yard import make_recipe
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'A')
+    return render(request, 'recipes/pages/home.html', context={'recipes':[make_recipe() for _ in range(8)]})
+
+def recipe(request):
+    return render(request, 'recipes/pages/recipe.html', context={
+        'recipe': make_recipe(),
+    })
+
