442571a8cc4fe016a7e12c523bcd09d041c01edc
Aslay Clevisson <aslayclei@gmail.com>
2022-04-04 19:22:31 -0300

    namespace added var(app_name)

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes' # This is used to namespace the urls. Ex.: recipes:home or recipes:recipe :D
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/', views.recipe, name='recipe'),
+    path('recipes/<int:pk>/', views.recipe, name='recipe'),
 ]
