99a29137fd5144a9d9fc3c28a66c7f430ee8d22e
Aslay Clevisson <aslayclei@gmail.com>
2022-04-05 23:48:49 -0300

    improvement: filters and recipe data consumption(models)

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from utils.recipe.yard import make_recipe
 
+from recipes.models import Recipe
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes':[make_recipe() for _ in range(8)]})
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
-def recipe(request):
+def recipe(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    if recipe.is_published is not True:
+        pass
+    
     return render(request, 'recipes/pages/recipe.html', context={
-        'recipe': make_recipe(),
-    })
-
+        'recipe': recipe, 'is_detail_page': True,})
+    
+def category(request, category_id):
+    recipes = Recipe.objects.filter(is_published=True, category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
