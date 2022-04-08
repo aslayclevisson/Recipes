516f977e7b850f7b248245deb277a080ab03e3a1
Aslay Clevisson <aslayclei@gmail.com>
2022-04-07 21:48:23 -0300

    add: new route

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'recipes' # This is used to namespace the urls. Ex.: recipes:home or recipes:recipe :D
+# This is used to namespace the urls. Ex.: recipes:home or recipes:recipe :D
+app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
     path('recipes/<int:pk>/', views.recipe, name='recipe'),
 ]
